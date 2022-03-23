a63ade889a4e81aa7b48157dd6830ba88ac85485
Woshiwuja <gabusif@outlook.it>
2022-03-23 17:08:49 +0100

    added more print statements

--- file_size_diff.py
+++ file_size_diff.py
@@ -12,13 +12,34 @@ with open(sys.argv[1], 'r') as a, open(sys.argv[2], 'r') as b:
         diff_size = a_size - b_size
         a_file_name = os.path.basename(a.name)
         b_file_name = os.path.basename(b.name)
-        print(a_file_name,"size:",a_size)
-        print(b_file_name,"size:",b_size)
-        print("size difference:",diff_size)
+        if (a_size/1024<1 or b_size/1024<1):
+            print(a_file_name,"size:",a_size,"bytes")
+            print(b_file_name,"size:",b_size,"bytes")
+            print("size difference:",diff_size,"bytes")
+
+        elif ((a_size/1024)/1024>=1 or (b_size/1024)/1024>=1):
+            print(a_file_name,"Size:",round((a_size/1024)/1024, 2),"Megabytes")
+            print(b_file_name,"Size:",round((b_size/1024)/1024, 2),"Megabytes")
+            print("Size difference:",round((diff_size/1024)/1024,2),"Megabytes")
+
+        elif (a_size/1024>=1 or b_size/1024>=1):
+            print(a_file_name,"Size:",round(a_size/1024, 2),"kilobytes")
+            print(b_file_name,"Size:",round(b_size/1024, 2),"kilobytes")
+            print("Size difference:",round(diff_size/1024,2),"kilobytes")
     else:
         diff_size = b_size - a_size
         a_file_name = os.path.basename(a.name)
         b_file_name = os.path.basename(b.name)
-        print(a_file_name,"size:",a_size)
-        print(b_file_name,"size:",b_size)
-        print("size difference:",diff_size)
+        if (a_size/1024<1 or b_size/1024<1):
+            print(a_file_name,"size:",a_size,"bytes")
+            print(b_file_name,"size:",b_size,"bytes")
+            print("size difference:",diff_size,"bytes")
+        elif ((a_size/1024)/1024>=1 or (b_size/1024)/1024>=1):
+            print(a_file_name,"Size:",round((a_size/1024)/1024, 2),"Megabytes")
+            print(b_file_name,"Size:",round((b_size/1024)/1024, 2),"Megabytes")
+            print("Size difference:",round((diff_size/1024)/1024,2),"Megabytes")
+        elif (a_size/1024>=1 or b_size/1024>=1):
+            print(a_file_name,"Size:",round(a_size/1024, 2),"kilobytes")
+            print(b_file_name,"Size:",round(b_size/1024, 2),"kilobytes")
+            print("Size difference:",round(diff_size/1024,2),"kilobytes")
+
